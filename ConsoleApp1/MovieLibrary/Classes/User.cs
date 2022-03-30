6ae5cba3882fb8d6cac799c35c60a31a3c105a6a
Darko Aceski <darkoaceski@yahoo.com>
2022-03-30 22:55:15 +0200

    Movie Store

---
+++ ConsoleApp1/MovieLibrary/Classes/User.cs
@@ -0,0 +1,15 @@
+﻿using MovieLibrary.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieLibrary.Classes
+{
+    public class User : Member
+    {
+        public int MemberNumber { get; set; }
+        public EnumSubscription TypeOfSubscription { get; set; }
+        public string[] Movie { get; set; }
+
+    }
+}
